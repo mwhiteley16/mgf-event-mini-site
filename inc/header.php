a48e85cc48f3c5916860c7c5c25fa6a6ac8ba378
Matt Whiteley <matt@whiteleydesigns.com>
2017-05-23 08:30:59 -0400

    Custom CSS addition to header.

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,95 @@
-<?php $custom_css = get_field( 'custom_css', 'option' );
-if( $custom_css ) : ?>
-     <style><?php echo $custom_css; ?></style>
+<?php
+$custom_css = get_field( 'custom_css', 'option' );
+$primary_color = get_field( 'primary_color', 'option' );
+$link_color = get_field( 'link_color', 'option' );
+$secondary_color = get_field( 'secondary_color', 'option' );
+function hex2rgba($color, $opacity = false) { // convert hex to rgba for opaque use
+
+	$default = 'rgb(0,0,0)';
+
+	//Return default if no color provided
+	if(empty($color))
+          return $default;
+
+	//Sanitize $color if "#" is provided
+        if ($color[0] == '#' ) {
+        	$color = substr( $color, 1 );
+        }
+
+        //Check if color has 6 or 3 characters and get values
+        if (strlen($color) == 6) {
+                $hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+        } elseif ( strlen( $color ) == 3 ) {
+                $hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+        } else {
+                return $default;
+        }
+
+        //Convert hexadec to rgb
+        $rgb =  array_map('hexdec', $hex);
+
+        //Check if opacity is set(rgba or rgb)
+        if($opacity){
+        	if(abs($opacity) > 1)
+        		$opacity = 1.0;
+        	$output = 'rgba('.implode(",",$rgb).','.$opacity.')';
+        } else {
+        	$output = 'rgb('.implode(",",$rgb).')';
+        }
+
+        //Return rgb(a) color string
+        return $output;
+}
+$primary_color_rgba = hex2rgba($primary_color, 0.8);
+if( $custom_css || $primary_color || $secondary_color ) : ?>
+     <style>
+          <?php echo $custom_css; ?>
+          a.register-now,
+          a.home-registration-link,
+          a.register-now:visited,
+          a.home-register-link:visited,
+          .schedule-date.active,
+		.schedule-item-inner .schedule-event-item-inner-con.green,
+		.color-key div.green {
+               background-color: <?php echo $link_color; ?>;
+               color: #fff;
+          }
+		.schedule-item-inner .schedule-event-item-inner-con.blue,
+		.color-key div.blue {
+			background-color: <?php echo $secondary_color; ?>;
+			color: #fff;
+		}
+          a, a:visited,
+          .schedule-date i.fa {
+               color: <?php echo $link_color; ?>;
+          }
+          ul#menu-main-menu > li:hover > a,
+          ul#menu-main-menu > li.current-menu-parent a {
+               color: #336699;
+               border-bottom: 7px solid <?php echo $primary_color; ?>;
+          }
+          .home-section-2 h1,
+          h2,
+          .team-member-name,
+          h1.page-header span {
+               color: <?php echo $primary_color; ?>;
+          }
+          .venue-content-box,
+          .team-member-img-con:hover {
+               background-color: <?php echo $primary_color_rgba; ?>;
+          }
+          .speaker-popup,
+          .schedule-date:hover,
+          .schedule-item-inner .schedule-event-item-inner-con.dark-blue,
+          .color-key div.dark-blue {
+               background-color: <?php echo $primary_color; ?>;
+          }
+          @media(max-width:1025px) {
+          .navigation {
+               background-color: <?php echo $primary_color; ?>;
+          }
+          }
+     </style>
 <?php endif; ?>
 
 <section class="header-con">
@@ -11,7 +100,7 @@ if( $custom_css ) : ?>
      </div>
 </section>
 
-<?php 
+<?php
 // check to make sure we're on the home page since we only want this functionality for that nav
 if( is_front_page() ) : ?>
 <script>
@@ -52,7 +141,7 @@ jQuery(document).ready(function() {
   jQuery('li.registration-menu-link a').click(function(event) {
      event.preventDefault();
      jQuery('.register-now-container').fadeIn();
-  });   
+  });
   // close it when you click the 'x'
   jQuery('i.close-registration').click(function() {
     jQuery('.register-now-container').fadeOut();
@@ -94,5 +183,3 @@ jQuery(document).mouseup(function (e) {
 });
 </script>
 <?php endif; ?>
-
-
